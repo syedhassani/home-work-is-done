af611716f84a554f75e4e1e0fb27dafbfc3b0a41
syedhassani <syedmhassani12@gmail.com>
2024-12-18 20:33:51 +1100

    home work

---
+++ main.py
@@ -0,0 +1,11 @@
+def sum(n):
+#     return n*(n+1)/2
+# print(sum(10))
+def arraysum(a):
+        sum=0
+        for i in a:
+            sum = sum + i
+            print("hello")
+        return(sum)
+a=[12,3,4,15,10,100,777]
+print(arraysum(a))
